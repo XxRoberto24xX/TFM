65494ad65a4c5412ea3566e85a62af8c2a610ca7
XxRoberto_24xX <gonzaleznavasroberto@gmail.com>
2026-05-28 19:00:40 +0200

    update in the database schema

--- database.sql
+++ database.sql
@@ -1,48 +1,147 @@
--- Migrations will appear here as you chat with AI
+-- MySQL dump 10.13  Distrib 8.4.6, for Win64 (x86_64)
+--
+-- Host: localhost    Database: gasstationprices
+-- ------------------------------------------------------
+-- Server version	8.4.6
 
-create table users (
-  email text primary key,
-  name text not null,
-  password text not null
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-create table cars (
-  plate text primary key,
-  consumption numeric not null,
-  brand text not null,
-  model text not null,
-  user_email text not null,
-  foreign key (user_email) references users (email)
-);
+--
+-- Table structure for table `cars`
+--
 
-create table gas_stations (
-  id bigint primary key generated always as identity,
-  brand text not null,
-  municipality text not null,
-  direction text not null,
-  hours text,
-  latitude double precision,
-  longitude double precision,
-  selling_type text
-);
+DROP TABLE IF EXISTS `cars`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cars` (
+  `plate` varchar(255) NOT NULL,
+  `brand` varchar(255) DEFAULT NULL,
+  `consumption` float DEFAULT NULL,
+  `model` varchar(255) DEFAULT NULL,
+  `user_email` varchar(255) NOT NULL,
+  PRIMARY KEY (`plate`),
+  KEY `FKccnubuwcd3sf8it7f3fl2baoe` (`user_email`),
+  CONSTRAINT `FKccnubuwcd3sf8it7f3fl2baoe` FOREIGN KEY (`user_email`) REFERENCES `users` (`email`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table favorite_gas_stations (
-  user_email text not null,
-  gas_station_id bigint not null,
-  primary key (user_email, gas_station_id),
-  foreign key (user_email) references users (email),
-  foreign key (gas_station_id) references gas_stations (id)
-);
+--
+-- Table structure for table `gas_stations`
+--
 
-create table gas_station_prices (
-  id bigint primary key generated always as identity,
-  gas_station_id bigint not null,
-  "timestamp" timestamptz not null default now(),
-  diesel_price numeric not null,
-  diesel_premium_price numeric not null,
-  gasoline_95_price numeric not null,
-  gasoline_98_price numeric not null,
-  foreign key (gas_station_id) references gas_stations (id)
-);
+DROP TABLE IF EXISTS `gas_stations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `gas_stations` (
+  `id` bigint NOT NULL,
+  `brand` varchar(255) DEFAULT NULL,
+  `direction` varchar(255) DEFAULT NULL,
+  `hours` varchar(255) DEFAULT NULL,
+  `latitude` double DEFAULT NULL,
+  `longitude` double DEFAULT NULL,
+  `municipality` varchar(255) DEFAULT NULL,
+  `selling_type` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create index on gas_station_prices using btree (gas_station_id, "timestamp" desc);
+--
+-- Table structure for table `prices`
+--
+
+DROP TABLE IF EXISTS `prices`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `prices` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `diesel` double DEFAULT NULL,
+  `gasoline95` double DEFAULT NULL,
+  `gasoline98` double DEFAULT NULL,
+  `gas_station_id` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UKguw1iuqbs7p2ulqaxpaiiw3ei` (`gas_station_id`,`date`),
+  KEY `idx_gasstation_date` (`gas_station_id`,`date`),
+  KEY `idx_prices_date` (`date`),
+  CONSTRAINT `FKqihlq70k7g01rge0lit5gql0y` FOREIGN KEY (`gas_station_id`) REFERENCES `gas_stations` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=11410 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_favorite_gas_stations`
+--
+
+DROP TABLE IF EXISTS `user_favorite_gas_stations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_favorite_gas_stations` (
+  `user_email` varchar(255) NOT NULL,
+  `gas_station_id` bigint NOT NULL,
+  PRIMARY KEY (`user_email`,`gas_station_id`),
+  KEY `fk_fav_gas_station` (`gas_station_id`),
+  CONSTRAINT `fk_fav_gas_station` FOREIGN KEY (`gas_station_id`) REFERENCES `gas_stations` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_fav_user` FOREIGN KEY (`user_email`) REFERENCES `users` (`email`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  PRIMARY KEY (`email`),
+  UNIQUE KEY `UKr43af9ap4edm43mmtq01oddj6` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping events for database 'gasstationprices'
+--
+/*!50106 SET @save_time_zone= @@TIME_ZONE */ ;
+/*!50106 DROP EVENT IF EXISTS `delete_old_prices` */;
+DELIMITER ;;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;;
+/*!50003 SET character_set_client  = cp850 */ ;;
+/*!50003 SET character_set_results = cp850 */ ;;
+/*!50003 SET collation_connection  = cp850_general_ci */ ;;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;;
+/*!50003 SET @saved_time_zone      = @@time_zone */ ;;
+/*!50003 SET time_zone             = 'SYSTEM' */ ;;
+/*!50106 CREATE*/ /*!50106 EVENT `delete_old_prices` ON SCHEDULE EVERY 1 DAY STARTS '2026-05-28 14:09:38' ON COMPLETION NOT PRESERVE ENABLE DO DELETE FROM prices
+  WHERE date < (CURRENT_DATE - INTERVAL 14 DAY) */ ;;
+/*!50003 SET time_zone             = @saved_time_zone */ ;;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;;
+/*!50003 SET character_set_results = @saved_cs_results */ ;;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;;
+DELIMITER ;
+/*!50106 SET TIME_ZONE= @save_time_zone */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-05-28 14:09:58

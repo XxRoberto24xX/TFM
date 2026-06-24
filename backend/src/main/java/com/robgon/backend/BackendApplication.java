825705bf847822718da82b5d5d82b731fa494fc1
XxRoberto_24xX <gonzaleznavasroberto@gmail.com>
2026-06-24 16:34:45 +0200

    add: a new environment file was added so the new passwords, keys and other sensible information stays save. A .env.template file was added for other developers to set their own environment files correctly.

--- backend/src/main/java/com/robgon/backend/BackendApplication.java
+++ backend/src/main/java/com/robgon/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package com.robgon.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -8,6 +9,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class BackendApplication {
 	public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+
 		SpringApplication.run(BackendApplication.class, args);
 	}
 }
